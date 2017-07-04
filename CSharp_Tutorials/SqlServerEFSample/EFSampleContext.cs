f274d93f836df4f08a01e48cf3ec92c72ceabc65
Larry Lai <yanlin.lai@afusion.com>
2017-07-04 10:32:29 +0800

    WOW! Using the Entity Framework ORM in .NET Framework is really convenient!

---
+++ CSharp_Tutorials/SqlServerEFSample/EFSampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace SqlServerEFSample
+{
+    public class EFSampleContext : DbContext
+    {
+        public EFSampleContext(string connectionString)
+        {
+            Database.SetInitializer<EFSampleContext>(new DropCreateDatabaseAlways<EFSampleContext>());
+            this.Database.Connection.ConnectionString = connectionString;
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
